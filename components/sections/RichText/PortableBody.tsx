3ace946916380baf2657a4eaeacb5e9f15d12602
vand-cloud <van.dvorak@gmail.com>
2026-09-18 15:21:58 +0200

    fix: recognize youtube.com/shorts/ links in blog embed

--- components/sections/RichText/PortableBody.tsx
+++ components/sections/RichText/PortableBody.tsx
@@ -23,6 +23,7 @@ function youtubeId(url: string): string | null {
     /(?:youtube\.com\/watch\?v=)([\w-]{11})/,
     /(?:youtu\.be\/)([\w-]{11})/,
     /(?:youtube\.com\/embed\/)([\w-]{11})/,
+    /(?:youtube\.com\/shorts\/)([\w-]{11})/,
   ];
   for (const re of patterns) {
     const m = url.match(re);
